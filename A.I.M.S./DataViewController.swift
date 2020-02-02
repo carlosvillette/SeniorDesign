f2152a9c6b35ad799a8acced27184a097a3d09b9
Guy Carlos Villette <guycarlosvillette@hon-2016238.home>
2020-02-02 17:03:32 -0500

    Initial Commit

---
+++ A.I.M.S./DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  A.I.M.S.
+//
+//  Created by Guy Carlos Villette on 2/2/20.
+//  Copyright © 2020 Guy Carlos Villette. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
